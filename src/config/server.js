2bee57d06b365b210f297ee5d91cd5fdcfb17e1b
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-03 19:16:43 -0300

    correção de lint e alteração do metodo start para receber porta por paremeto

--- src/config/server.js
+++ src/config/server.js
@@ -1,24 +1,25 @@
 const http = require('http')
 const app = require('./app')
 
-const PORT = process.env.PORT || 3000
 const server = http.createServer(app)
+server.on('listening', onListening)
+server.on('error', onError)
+
+function start (port) {
+  server.listen(port)
+}
 
 function onListening () {
-  const { address, port } = server.address()
+  const {
+    address,
+    port
+  } = server.address()
   console.log(`listenig in ${address}:${port}`)
 }
 
 function onError (error) {
   console.log(error)
 }
-
-function start () {
-  server.listen(PORT)
-}
-
-server.on('listening', onListening)
-server.on('error', onError)
 
 module.exports = {
   start
