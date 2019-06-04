622b3b800c13e3d7cdde3537085c144c9071993d
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-04 14:55:59 -0300

    setup acl

--- src/config/app.js
+++ src/config/app.js
@@ -6,6 +6,7 @@ const express = require('express')
 const compression = require('compression')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
+const acl = require('express-acl')
 const swaggerUi = require('swagger-ui-express')
 const cors = require('cors')
 
@@ -18,12 +19,24 @@ const {
 
 const app = express()
 
+acl.config({
+  baseUrl: '/api',
+  path: 'src/config'
+})
+
 app.use(cors())
 app.use(logging())
 app.use(helmet())
 app.use(compression())
 app.use(bodyParser.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(acl.authorize.unless({
+  path: [
+    '/api/auth/sign_in',
+    '/api/auth/sign_up',
+    '/api/auth/recovery_password'
+  ]
+}))
 app.use('/api', routes)
 app.use(serverError())
 
