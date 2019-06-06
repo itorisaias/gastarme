8ce40155e808d47b78151b2c6092f62d750d9816
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-06 13:57:45 -0300

    validação de request

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
+const { schemaValidatorLogin, validate } = require('./schemasValidators')
 const AuthController = require('../controllers/auth')
 const AuthService = require('../services/auth')
 const { User } = require('../database/models')
@@ -7,7 +8,7 @@ const { User } = require('../database/models')
 const authController = new AuthController(User, new AuthService(User))
 
 router
-  .post('/sign_in', (req, res, next) => authController.signIn(req, res, next))
+  .post('/sign_in', validate(schemaValidatorLogin), (req, res, next) => authController.signIn(req, res, next))
   .post('/sign_up', (req, res, next) => authController.signUp(req, res, next))
 
 router
