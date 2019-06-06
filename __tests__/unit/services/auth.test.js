bcc90701765e91edc127e141044b8d5b078fbd0e
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-06 14:02:59 -0300

    refatoração do teste unitario para service de autenticação

--- __tests__/unit/services/auth.test.js
+++ __tests__/unit/services/auth.test.js
@@ -9,47 +9,58 @@ describe('Service: Auth', () => {
 
   describe('authenticate', () => {
     it('should autenticate a user', async () => {
-      const userFake = await factore.create('User')
-      const userModelFake = {
-        findOne: jest.fn().mockResolvedValue(userFake)
+      const user = await factore.create('User')
+      const userModelMock = {
+        findOne: jest.fn().mockResolvedValue(user)
       }
-      const data = {}
-      const authService = new AuthService(userModelFake)
+      const authService = new AuthService(userModelMock)
 
       authService
-        .authenticate(data)
-        .then(result => expect(result).toEqual(userFake))
+        .authenticate(user)
+        .then(result => expect(result).toEqual(user))
     })
-    it('should return false when the password does not match', () => {
-      const userModelFake = {
+    it('should return false when the password does not match', async () => {
+      const user = await factore.create('User', {
+        password: '123456'
+      })
+      const userModelMock = {
+        findOne: jest.fn().mockResolvedValue(user)
+      }
+      const authService = new AuthService(userModelMock)
+
+      authService
+        .authenticate({ password: '654321' })
+        .then(result => expect(result).toBe(false))
+    })
+    it('should return false when not found user', () => {
+      const userModelMock = {
         findOne: jest.fn().mockResolvedValue()
       }
-      const data = {}
-      const authService = new AuthService(userModelFake)
+      const authService = new AuthService(userModelMock)
 
       authService
-        .authenticate(data)
+        .authenticate({})
         .then(result => expect(result).toBe(false))
     })
   })
   describe('recoveryPassword', () => {
     it('should recovery password', async () => {
       const userFake = await factore.create('User')
-      const userModelFake = {
+      const userModelMock = {
         findOne: jest.fn().mockResolvedValue(userFake),
         update: jest.fn().mockResolvedValue([1])
       }
-      const authService = new AuthService(userModelFake)
+      const authService = new AuthService(userModelMock)
 
       authService
         .recoveryPassword(userFake.email)
-        .then(result => expect(result).toHaveLength(1))
+        .then(result => expect(result).toBe(true))
     })
     it('should return false when the email not found', () => {
-      const userModelFake = {
+      const userModelMock = {
         findOne: jest.fn().mockResolvedValue()
       }
-      const authService = new AuthService(userModelFake)
+      const authService = new AuthService(userModelMock)
 
       authService
         .recoveryPassword(null)
